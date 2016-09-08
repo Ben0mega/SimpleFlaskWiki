14aad008ce8de8f230dfd4108b0ea4469a9eb5d8
Ben Mehne <bmehne@gmail.com>
2016-09-07 17:44:06 -0700

    Added binary upload functionality - currently limited to three image types

--- main.py
+++ main.py
@@ -1,78 +1,109 @@
 import markdown
+import glob
 import string
 import html
 import os.path
-from flask import Flask,render_template,Markup, request, redirect, url_for
+import errno
+import os
+from flask import Flask,render_template,Markup, request, redirect, url_for, send_file
 
 app = Flask(__name__)
 
 @app.route('/pages/<name>')
 def wiki_page(name):
-	file_name = name.split('.')[0]
-	title = fileNameToTitle(file_name)
-	#TODO: once we support binary files, turn this into a try-catch
-	# and handle the case that it is not a markdown
-	if isMarkdown(file_name):
-		content = fileNameToHTML(file_name)
-		return render_template('wiki_page.html', content=content, title=title)
+	title = nameToTitle(name)
+	file_ = nameToFileName(name)
+	return render(title, file_)
+
+def render(title, file_name):
+	fn = getRender(getExtension(file_name))
+	return fn(title, file_name)
+
+def markDownRender(title, file_name):
+	with open(file_name) as f:
+		content = f.read()
+	content = Markup(markdown.markdown(content))
+	return render_template('wiki_page.html', content=content, title=title)
+
+def binaryRender(title, file_name):
+	return send_file(file_name)
+
+def nameToTitle(name):
+	name = sanitize(name)
+	return name.replace('-', ' ')
+
+# get file_name for name.  if content_type is specified, verify that the extension is correct
+def nameToFileName(name, content_type=None):
+	name = sanitize(name)
+	files = glob.glob(os.path.join('pages', name+'.*'))
+	assert(len(files) < 2)
+	if len(files) == 0:
+		if content_type == None:
+			raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), name)
+		# create name based on content type
+		print(content_type)
+		fname = os.path.join('pages', name+'.'+contentToExtension(content_type))
+		files=[fname]
+	if content_type != None:
+		assert(getExtension(files[0]) == contentToExtension(content_type))
+	return files[0]
+
+def contentToExtension(content_type):
+	if content_type == 'image/gif':
+		extension = 'gif'
+	elif content_type == 'image/png':
+		extension = 'png'
+	elif content_type == 'image/jpeg':
+		extension = 'jpeg'
+	elif content_type == 'text/plain':
+		extension='md'
 	else:
-		# this should just fail at runtime...
-		return send_file(os.path.join('pages',file_name+'.binary'))
+		extension = None
 
-# names go like this:
-# ' ' -> '_'
-# '_' -> '__'
-# note that '___' will be '_ ', not ' _' or anything else
-# not sure if I care enough to fix this issue...
+	assert(getRender(extension) != None)
+	return extension
 
-def fileNameToTitle(fn):
-	fn = stripNonPrintable(fn)
-	fn = fn.replace('__', '\0')
-	fn = fn.replace('_', ' ')
-	fn = fn.replace('\0', '_')
+def isBinary(fn):
+	ext = getExtension(fn)
+	return getRender(ext) == binaryRender
+
+def getExtension(fn):
+	return fn.split('.')[-1]
+
+def getRender(name_type):
+	names_to_renderers = {
+			'md' : markDownRender,
+			'gif' : binaryRender,
+			'jpeg' : binaryRender,
+			'png' : binaryRender,
+		}
+	fn = names_to_renderers.get(name_type, None)
 	return fn
 
-def titleToFileName(fn):
-	fn = stripNonPrintable(fn)
-	fn = fn.replace('_', '\0')
-	fn = fn.replace(' ', '_')
-	fn = fn.replace('\0', '__')
-	return fn
+def sanitize(name):
+	def safe(a):
+		if a in string.ascii_letters or a in string.digits or a =='_' or a=='-':
+			return a
+		return ''
+	return ''.join(safe(a) for a in name)
 
-def stripNonPrintable(fn):
-	out = ''
-	for a in fn:
-		if a not in string.printable or a in '\t\n\r\x0b\x0c':
-			continue
-		out+=a
-	return out
-
-test_fn = "hello world_of wiki"
-assert(test_fn == fileNameToTitle(titleToFileName(test_fn)))
-
-
-def fileNameToHTML(fn):
-	content = fileNameToMarkDown(fn)
-	return Markup(markdown.markdown(content))
-
-def fileNameToMarkDown(fn, dirs='pages'):
-	full_path = os.path.join(dirs, fn+'.md')
-	return fileNameToContent(full_path)
-
-def fileNameToContent(fn):
-	with open(fn) as f:
-		return f.read()
 
 #@app.route('/edit/<name>/<revision>') # for previous revisions
 @app.route('/edit/')
 @app.route('/edit/<name>')
 def edit_page(name=None, revision=None):
-	file_name = name.split('.')[0]
-	title = fileNameToTitle(file_name)
-	if isMarkdown(file_name):
-		content = html.escape(fileNameToMarkDown(file_name))
-	else:
+	title = nameToTitle(name)
+	try:
+		file_name = nameToFileName(name)
+	except FileNotFoundError:
+		file_name = "SENTINEL.jpeg"
+
+	if isBinary(file_name):
 		content = "BINARY/UNKNOWN/EMPTY FILE"
+	else:
+		with open(file_name) as f:
+			content = f.read()
+		content = html.escape(content)
 	return render_template('edit_page.html', content=content, title=title)
 
 def get_revisions(file_name):
@@ -88,25 +119,29 @@ def get_revision_content(file_name, revision_id):
 def save_page():
 	name = request.form['title']
 	content = request.form.get('content', None)
-	fn = titleToFileName(name)
-	if 'file' not in request.files:
+
+	try:
+		fn = nameToFileName(name)
+	except FileNotFoundError:
+		# assume it is a markdown file for now...
+		fn = nameToFileName(name, 'text/plain')
+
+	file_ = request.files.get('file')
+	if not file_:
 		save_content(fn, content)
-		return redirect(url_for('wiki_page', name=fn))
-	files = request.files['file']
-	if files.filename == '':
-		save_content(fn, content)
-		return redirect(url_for('wiki_page', name=fn))
-	files.save(os.path.join('pages', fn+'.binary'))
-	return redirect(url_for('wiki_page', name=fn))
+		return redirect(url_for('wiki_page', name=name))
+
+	# use the actual mimetype since we now have a file...
+	fn = nameToFileName(name, file_.mimetype)
+	save_content(fn, file_)
+	return redirect(url_for('wiki_page', name=name))
 
 def save_content(fn, content):
-	full_path = os.path.join('pages', fn+'.md')
-	with open(full_path, 'w') as f:
-		f.write(content)
-
-def isMarkdown(fn):
-	#TODO: support non-markdown files...
-	return True
+	if hasattr(content, 'save'):
+		content.save(fn)
+	else:
+		with open(fn, 'w') as f:
+			f.write(content)
 
 if __name__ == "__main__":
 	app.run()
